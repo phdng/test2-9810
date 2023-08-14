a34cbce2ebc1ed6d4ca8dce93b1e4afb3a99e44d
xxrishikcooTR <xxrishikcooTR@gmail.com>
2023-08-14 03:17:09 -0700

    Finalize Things For Git Push #IGNORE

--- mod_defconfig.sh
+++
@@ -1,9 +0,0 @@
-if [ $1 = ] 2> /dev/null
-then
-echo Enter SOME_VALUE=VALUE To Mod 
-else
-echo $1 >> arch/arm64/configs/cuttlefish_defconfig
-echo $1 >> arch/arm64/configs/exynos9810-starlte_defconfig
-echo $1 >> arch/arm64/configs/exynos9810-star2lte_defconfig
-echo $1 >> arch/arm64/configs/exynos9810-crownlte_defconfig
-fi
